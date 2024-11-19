1341cabfd7accc56a377161ce8f936a9d41667c7
Kumar-Ankit543 <kumarankit7613@gmail.com>
2024-11-19 19:43:04 +0530

    Bootstraped the project

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@repo/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="text-2xl">hi there...</div>
+      <Button className="text-3xl" appName="btn">
+        Hello
+      </Button>
+    </div>
+  );
+}
