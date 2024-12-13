508b0db9c08f9149634653351304ed230fdb2db7
Kumar-Ankit543 <kumarankit7613@gmail.com>
2024-12-13 10:01:02 +0530

    added locking to the DB

--- apps/user-app/app/lib/actions/p2pTransfer.ts
+++ apps/user-app/app/lib/actions/p2pTransfer.ts
@@ -22,6 +22,7 @@ export async function p2pTransfer(to: string, amount: number) {
     };
   }
   await prisma.$transaction(async (tx) => {
+    await tx.$queryRaw`SELECT * FROM "Balance" WHERE "userId" = ${Number(fromUser)} FOR UPDATE`;
     const fromBalance = await tx.balance.findUnique({
       where: {
         userId: Number(fromUser),
